613d1ae73afa693127db60726141a0bcc0256aee
Tony Rat <tonyrat@tonys-air.ait-sydney.net.au>
2018-05-04 15:26:42 +1000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :profiles
+  resources :products
+  devise_for :users
+  devise_for :views
+  root :to => "products#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20180503231252_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.integer :price
+      t.integer :user
+
+      t.timestamps
+    end
+  end
+end
