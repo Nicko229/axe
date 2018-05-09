ffff4e8de994eaf4471a5635e6a27f3e5706e8dd
Tony Rat <tonyrat@tonys-air.ait-sydney.net.au>
2018-05-09 11:05:41 +1000

    sendgrid welcome

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['YOUR_SENDGRID_USERNAME'],
+  :password => ENV['YOUR_SENDGRID_PASSWORD'],
+  :domain => ENV['SENDGRID_DOMAIN'],
+  :address => ENV['SENDGRID_ADDRESS'],
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
